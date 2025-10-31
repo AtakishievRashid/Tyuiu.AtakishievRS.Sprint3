f85c24586c63a2af48623219e380fb5cf37da4b9
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-11-01 00:47:38 +0500

    Task4

---
+++ Tyuiu.AtakishievRS.Sprint3.Task4.V16.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.AtakishievRS.Sprint3.Task4.V16.Lib;
+namespace Tyuiu.AtakishievRS.Sprint3.Task4.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService dataService = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            Assert.AreEqual(6.719, dataService.Calculate(startValue, stopValue)); ;
+        }
+    }
+}
---
+++ Tyuiu.AtakishievRS.Sprint3.Task4.V16/Program.cs
@@ -0,0 +1,8 @@
+﻿using Tyuiu.AtakishievRS.Sprint3.Task4.V16.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
