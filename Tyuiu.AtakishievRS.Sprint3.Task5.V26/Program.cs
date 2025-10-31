b95835c50e5d9781a52fca15334057f81e16604d
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-11-01 01:02:53 +0500

    Task5

---
+++ Tyuiu.AtakishievRS.Sprint3.Task5.V26/Program.cs
@@ -0,0 +1,8 @@
+﻿using Tyuiu.AtakishievRS.Sprint3.Task5.V26.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
